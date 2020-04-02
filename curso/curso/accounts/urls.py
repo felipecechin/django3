ea322b5431f399afbd589a6d54da947cf7422b24
Felipe Cechin <ficechin@hotmail.com>
2020-04-02 02:12:12 -0300

    Seção 4 em andamento.

--- curso/curso/accounts/urls.py
+++ curso/curso/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from django.contrib.auth import views as auth_views
-from .views import register
+from .views import *
 
 urlpatterns = [
+    path('', dashboard, name='dashboard'),
     path('entrar/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('cadastre-se/', register, name='register'),
+    path('sair/', auth_views.LogoutView.as_view(next_page='core:home'), name='logout'),
 ]
-
